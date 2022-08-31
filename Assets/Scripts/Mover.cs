52151e49f66d34552173fb80417a4884ef33f8b5
LAPTOP-BKNJG3GA\13177 <zhang996@morrisville.edu>
2022-08-31 13:51:20 -0400

    Serialize Field

---
+++ Assets/Scripts/Mover.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Mover : MonoBehaviour
+{
+    [SerializeField] float xValue = 0f;
+    [SerializeField] float yValue = 0f;
+    [SerializeField] float zValue = 0f;    
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Translate(xValue,yValue,zValue);
+    }
+}
