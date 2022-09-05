e79c2d9029f9f82ef448de4d19c90850c86f2004
LAPTOP-BKNJG3GA\13177 <zhang996@morrisville.edu>
2022-09-05 13:19:09 -0400

    Time.deltaTime()

--- Assets/Scripts/Mover.cs
+++ Assets/Scripts/Mover.cs
@@ -6,15 +6,18 @@ public class Mover : MonoBehaviour
 {
     [SerializeField] float xValue = 0f;
     [SerializeField] float yValue = 0f;
-    [SerializeField] float zValue = 0f;    
+    [SerializeField] float zValue = 0f;
+    [SerializeField] float movSpeed = 10f;
     void Start()
     {
         
     }
 
-    // Update is called once per frame
     void Update()
     {
-        transform.Translate(xValue,yValue,zValue);
+        float xValue = Input.GetAxis("Horizontal")*Time.deltaTime*movSpeed;
+        float zValue = Input.GetAxis("Vertical")*Time.deltaTime*movSpeed;
+        transform.Translate(xValue,0,zValue);
+
     }
 }
